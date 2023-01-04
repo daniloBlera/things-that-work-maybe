a8af48b70bcca69849920667a6282f51c32d71c3
Danilo Blera <danilo.cblera@protonmail.com>
2023-01-04 17:07:32 -0300

    Improves ngrams module documentaion

--- python/autocomplete-with-ngrams/ngrams/__init__.py
+++ python/autocomplete-with-ngrams/ngrams/__init__.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
-# Restricting the number of importable kludges to the minimum while avoiding
-# taxonomies is a good thing
+"""This module provides a simple word-based autocompletion using
+n-gram language models.
+
+The implementation was based on the contents from the second week of
+coursera's "Natural Language Processing with Probabilistic Models"
+course, as well as the chapter three of the "Speech and Language
+Processing" book.
+
+To use this module, import the four functions below from its top
+module namespace:
+
+    * get_tokenized_sentences_from
+    * preprocess_data
+    * count_ngrams
+    * get_suggestions
+
+The rest of the symbols and kludges found in this module are for
+internal usage mostly because avoiding namespace taxonomies in general
+is a good thing.
+"""
 from ngrams.tools import get_tokenized_sentences_from, preprocess_data, \
-    get_suggestions, count_ngrams
+    count_ngrams, get_suggestions
