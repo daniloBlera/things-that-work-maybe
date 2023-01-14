200c22cf2adf840dd445a69be1447fd53c9c08e0
Danilo Blera <danilo.cblera@protonmail.com>
2023-01-14 05:22:44 -0300

    Updates gitignore and adds python closure example

---
+++ python/closures/configuring_downloader.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""An example of closure usage in python"""
+import nltk
+
+
+def make_downloader(download_path):
+    """Return a download function configured with a download path"""
+    def dl_func(package):
+        nltk.download(package, download_dir=download_path)
+
+    return dl_func
+
+
+# assign to `download` a download function configured to
+# use './nltk-resources' as the download directory
+download = make_downloader('./nltk-resources')
+
+# download the 'punkt', 'twitter_samples', and 'stopwords'
+# packages with the previously configured download function
+download('punkt')
+download('twitter_samples')
+download('stopwords')
