b6be3b735afe5d2b2b21fb7902db3b06414f8df2
Danilo Blera <danilo.cblera@protonmail.com>
2023-03-28 15:12:41 -0300

    Small changes to python snippets

--- python/decorators/time_logging.py
+++ python/decorators/time_logging.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Wrap functions with a timestamp logger"""
 from datetime import datetime
 
 
--- python/function-composition/compose.py
+++ python/function-composition/compose.py
@@ -1,52 +1,34 @@
 #!/usr/bin/env python3
 """Exercising bad ideas for the sake of learning"""
-from collections import Counter
-from itertools import pairwise
 from functools import reduce
 
 
-def composeN(*funcs):
-    """Compose an arbitrary number of functions"""
-    def compose2(f, g):
-        return lambda *args, **kwargs: f(g(*args, **kwargs))
+# %% debugging function order
+def tee(func):
+    """Print the input args and output of `func`, kinda like `tee`"""
+    def wrapper(*args, **kwargs):
+        output = func(*args, **kwargs)
+        print(f'* {func.__name__}({args=}, {kwargs=}) => {output}')
+        return output
+    return wrapper
 
-    return reduce(compose2, funcs)
 
+# %% composition implementation
+def compose(f, g, *rest, left_to_right=False):
+    funcs = [f, g, *rest]
 
-def compose(*funcs):
-    """compose(f1, f2, ..., fN) => f1 . f2 . ... . fN
-
-    Same as the composeN function but more compact and harder to read
-    """
     return reduce(lambda f, g: lambda *args, **kwargs: f(g(*args, **kwargs)),
-                  funcs)
+                  funcs if not left_to_right else reversed(funcs))
 
 
-comp = compose(int, float, str, float, int)
-print(f'{comp(1) == 1 = }')
+# %% tee-wrapped functions
+intt = tee(int)
+floatt = tee(float)
 
+# %% testing compose(f1, f2, f3)(x) == f1(f2(f3(x)))
+pipeline = compose(intt, floatt)
+pipeline(1.9)
 
-# Some examples that you should probably avoid
-def flatten(xss):
-    """Flatten an arbitrarily nested list
-
-    Implementation based on the `flatten` function from the
-    chapter 4 of the book "On Lisp".
-    """
-    def rec(x, acc):
-        if not isinstance(x, list):
-            return [x] + acc
-        elif len(x) == 0:
-            return acc
-        else:
-            [head, *tail] = x
-            return rec(head, rec(tail, acc))
-
-    return rec(xss, [])
-
-
-# List-fying a pairwise generator because why not?
-pairwise_l = compose(list, pairwise)
-
-# Flatten nested list then count elements
-flatten_and_count = compose(Counter, flatten)
+# %% testing compose(f1, f2, f3)(x) == f3(f2(f1(x)))
+pipeline = compose(intt, floatt, left_to_right=True)
+pipeline(1.9)
